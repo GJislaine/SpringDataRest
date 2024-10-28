ee04363982bca0046d592d164b50e2ebea28cae3
GJislaine <jislainebennani@gmail.com>
2024-10-28 14:31:20 +0100

    com

--- SpringDataRest/src/main/java/com/example/SpringDataRest/web/VoitureController.java
+++ SpringDataRest/src/main/java/com/example/SpringDataRest/web/VoitureController.java
@@ -3,8 +3,10 @@ package com.example.SpringDataRest.web;
 import com.example.SpringDataRest.modele.Voiture;
 import com.example.SpringDataRest.modele.VoitureRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 public class VoitureController {
